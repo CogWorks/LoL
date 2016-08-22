a6f4bf40c9947774901f6fa617b7537a1a9475ca
mddsangster <matthew.donald.sangster@gmail.com>
2016-08-22 19:06:24 -0400

    for modulization

---
+++ lol/__init__.py
@@ -0,0 +1,9 @@
+import LoL
+import credentials
+import utils
+
+__title__ = 'LoL'
+__version__ = '1.0.1'
+__author__ = 'mdds'
+__license__ = 'RPI'
+__copyright__ = 'Copyright (c) 2016 CogWorks <https://github.com/CogWorks>'
